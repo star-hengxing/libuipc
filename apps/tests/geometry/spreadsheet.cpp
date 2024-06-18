60b551d11dacf5fd83f2eb7b4cf5808c12eb4fd7
MuGdxy <lxy819469559@gmail.com>
2024-06-18 16:24:06 +0800

    improve scene io

--- apps/tests/geometry/spreadsheet.cpp
+++ apps/tests/geometry/spreadsheet.cpp
@@ -8,28 +8,38 @@
 #include <uipc/geometry/utils/extract_surface.h>
 #include <uipc/geometry/utils/flip_inward_triangles.h>
 
-#include <fstream>
 using namespace uipc;
 using namespace uipc::geometry;
 TEST_CASE("spreadsheet", "[io]")
 {
     SimplicialComplexIO io;
 
-    auto mesh = io.read_msh(fmt::format("{}cube.msh", AssetDir::tetmesh_path()));
+    auto mesh =
+        io.read_msh(fmt::format("{}cylinder_hole.msh", AssetDir::tetmesh_path()));
 
     label_surface(mesh);
     label_triangle_orient(mesh);
 
-    auto flipped = flip_inward_triangles(mesh);
+    // create an index attribute
+    auto Is      = mesh.vertices().create<IndexT>("Index", 0);
+    auto Is_view = view(*Is);
+    std::iota(Is_view.begin(), Is_view.end(), 0);
+
+    mesh.meta().create<std::string>("name", "cylinder_hole");
+
+    auto flipped      = flip_inward_triangles(mesh);
     auto mesh_surface = extract_surface(flipped);
 
     SpreadSheetIO sio{AssetDir::output_path()};
 
     // dump to csv
-    sio.write_csv("test_spreadsheet", mesh);
-
+    sio.write_csv("spreadsheet", mesh);
     // dump to json
-    sio.write_json("test_spreadsheet", mesh);
+    sio.write_json("spreadsheet", mesh);
 
-    sio.write_csv(mesh_surface);
+    // dump the surface to csv
+    sio.write_csv("spreadsheet_surf", mesh_surface);
+
+    // write the surface to obj
+    io.write_obj(fmt::format("{}cylinder_hole.obj", AssetDir::output_path()), mesh_surface);
 }
