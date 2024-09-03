f925c17fd629781f9b6a15ca0616e1861eda161a
MuGdxy <lxy819469559@gmail.com>
2024-09-03 09:40:05 +0800

    fix scene io, to support particle/rod/shell/volume

--- src/uipc/geometry/utils/merge.cpp
+++ src/uipc/geometry/utils/merge.cpp
@@ -26,8 +26,8 @@ namespace detail
 
         SimplicialComplex R;
 
-        vector<SizeT> vertex_offsets(complexes.size());
-        vector<SizeT> vertex_counts(complexes.size());
+        vector<SizeT> vertex_offsets(complexes.size() + 1, 0);
+        vector<SizeT> vertex_counts(complexes.size() + 1, 0);
 
         std::ranges::transform(complexes,
                                vertex_counts.begin(),
@@ -39,23 +39,23 @@ namespace detail
 
 
         {  // 1) merge vertices
-            SizeT num_verts = vertex_offsets.back() + vertex_counts.back();
+            SizeT num_verts = vertex_offsets.back();
 
             R.vertices().resize(num_verts);
 
-            for(SizeT dst_offset = 0; auto complex : complexes)
+            for(auto&& [i, complex] : enumerate(complexes))
             {
-                auto count = complex->vertices().size();
+                auto count  = vertex_counts[i];
+                auto offset = vertex_offsets[i];
                 R.vertices().copy_from(complex->vertices(),
-                                       AttributeCopy::range(dst_offset, 0, count));
-                dst_offset += count;
+                                       AttributeCopy::range(offset, 0, count));
             }
         }
 
         {  // 2) merge edges
 
-            vector<SizeT> edge_offsets(complexes.size());
-            vector<SizeT> edge_counts(complexes.size());
+            vector<SizeT> edge_offsets(complexes.size() + 1, 0);
+            vector<SizeT> edge_counts(complexes.size() + 1, 0);
 
             std::ranges::transform(complexes,
                                    edge_counts.begin(),
@@ -76,7 +76,7 @@ namespace detail
             std::exclusive_scan(
                 edge_counts.begin(), edge_counts.end(), edge_offsets.begin(), 0);
 
-            SizeT num_edges = edge_offsets.back() + edge_counts.back();
+            SizeT num_edges = edge_offsets.back();
 
             R.edges().resize(num_edges);
 
@@ -84,39 +84,42 @@ namespace detail
 
 
             // setup edge topology
-            for(auto [I, complex] : enumerate(complexes))
+            for(auto&& [I, complex] : enumerate(complexes))
             {
                 SizeT e_offset = edge_offsets[I];
                 SizeT e_count  = edge_counts[I];
                 SizeT v_offset = vertex_offsets[I];
 
-                span<Vector2i>       dst_Es = Es.subspan(e_offset, e_count);
-                span<const Vector2i> src_Es = complex->edges().topo().view();
+                if(e_count)
+                {
+                    span<Vector2i> dst_Es = Es.subspan(e_offset, e_count);
+                    span<const Vector2i> src_Es = complex->edges().topo().view();
 
-                // add offset to each edge
-                std::ranges::transform(src_Es,
-                                       dst_Es.begin(),
-                                       [=](const Vector2i& e) -> Vector2i
-                                       { return e.array() + v_offset; });
+                    // add offset to each edge
+                    std::ranges::transform(src_Es,
+                                           dst_Es.begin(),
+                                           [=](const Vector2i& e) -> Vector2i
+                                           { return e.array() + v_offset; });
+                }
             }
 
             // copy edge attributes
-            for(SizeT dst_offset = 0; auto complex : complexes)
+            for(auto&& [I, complex] : enumerate(complexes))
             {
-                auto count = complex->edges().size();
+                auto count  = edge_counts[I];
+                auto offset = edge_offsets[I];
                 if(count > 0)
                 {
                     R.edges().copy_from(complex->edges(),
-                                        AttributeCopy::range(dst_offset, 0, count));
+                                        AttributeCopy::range(offset, 0, count));
                 }
-                dst_offset += count;
             }
         }
 
         {  // 3) merge triangles
 
-            vector<SizeT> triangle_offsets(complexes.size());
-            vector<SizeT> triangle_counts(complexes.size());
+            vector<SizeT> triangle_offsets(complexes.size() + 1, 0);
+            vector<SizeT> triangle_counts(complexes.size() + 1, 0);
 
             std::ranges::transform(complexes,
                                    triangle_counts.begin(),
@@ -133,12 +136,13 @@ namespace detail
                                            return complex->triangles().size();
                                        }
                                    });
+
             std::exclusive_scan(triangle_counts.begin(),
                                 triangle_counts.end(),
                                 triangle_offsets.begin(),
                                 0);
 
-            SizeT num_triangles = triangle_offsets.back() + triangle_counts.back();
+            SizeT num_triangles = triangle_offsets.back();
 
             R.triangles().resize(num_triangles);
 
@@ -147,37 +151,39 @@ namespace detail
             // setup triangle topology
             for(auto [I, complex] : enumerate(complexes))
             {
-                SizeT t_offset = triangle_offsets[I];
-                SizeT t_count  = triangle_counts[I];
+                SizeT offset   = triangle_offsets[I];
+                SizeT count    = triangle_counts[I];
                 SizeT v_offset = vertex_offsets[I];
 
-                span<Vector3i> dst_Ts = Fs.subspan(t_offset, t_count);
-                span<const Vector3i> src_Ts = complex->triangles().topo().view();
+                if(count)
+                {
+                    span<Vector3i> dst_Ts = Fs.subspan(offset, count);
+                    span<const Vector3i> src_Ts = complex->triangles().topo().view();
 
-                // add offset to each triangle
-                std::ranges::transform(src_Ts,
-                                       dst_Ts.begin(),
-                                       [=](const Vector3i& t) -> Vector3i
-                                       { return t.array() + v_offset; });
+                    // add offset to each triangle
+                    std::ranges::transform(src_Ts,
+                                           dst_Ts.begin(),
+                                           [=](const Vector3i& t) -> Vector3i
+                                           { return t.array() + v_offset; });
+                }
             }
 
             // copy triangle attributes
-            for(SizeT dst_offset = 0; auto complex : complexes)
+            for(auto [I, complex] : enumerate(complexes))
             {
-                auto count = complex->triangles().size();
+                auto count = triangle_counts[I];
                 if(count > 0)
                 {
                     R.triangles().copy_from(complex->triangles(),
-                                            AttributeCopy::range(dst_offset, 0, count));
+                                            AttributeCopy::range(triangle_offsets[I], 0, count));
                 }
-                dst_offset += count;
             }
         }
 
         {  // 4) merge tetrahedra
 
-            vector<SizeT> tetrahedron_offsets(complexes.size());
-            vector<SizeT> tetrahedron_counts(complexes.size());
+            vector<SizeT> tetrahedron_offsets(complexes.size() + 1, 0);
+            vector<SizeT> tetrahedron_counts(complexes.size() + 1, 0);
 
             std::ranges::transform(complexes,
                                    tetrahedron_counts.begin(),
@@ -200,8 +206,7 @@ namespace detail
                                 tetrahedron_offsets.begin(),
                                 0);
 
-            SizeT num_tetrahedra =
-                tetrahedron_offsets.back() + tetrahedron_counts.back();
+            SizeT num_tetrahedra = tetrahedron_offsets.back();
 
             R.tetrahedra().resize(num_tetrahedra);
 
@@ -214,26 +219,29 @@ namespace detail
                 SizeT t_count  = tetrahedron_counts[I];
                 SizeT v_offset = vertex_offsets[I];
 
-                span<Vector4i> dst_Ts = Ts.subspan(t_offset, t_count);
-                span<const Vector4i> src_Ts = complex->tetrahedra().topo().view();
+                if(t_count)
+                {
+                    span<Vector4i> dst_Ts = Ts.subspan(t_offset, t_count);
+                    span<const Vector4i> src_Ts = complex->tetrahedra().topo().view();
 
-                // add offset to each tetrahedron
-                std::ranges::transform(src_Ts,
-                                       dst_Ts.begin(),
-                                       [=](const Vector4i& t) -> Vector4i
-                                       { return t.array() + v_offset; });
+                    // add offset to each tetrahedron
+                    std::ranges::transform(src_Ts,
+                                           dst_Ts.begin(),
+                                           [=](const Vector4i& t) -> Vector4i
+                                           { return t.array() + v_offset; });
+                }
             }
 
             // copy tetrahedron attributes
-            for(SizeT dst_offset = 0; auto complex : complexes)
+            for(auto [I, complex] : enumerate(complexes))
             {
-                auto count = complex->tetrahedra().size();
+                auto count = tetrahedron_counts[I];
                 if(count > 0)
                 {
-                    R.tetrahedra().copy_from(complex->tetrahedra(),
-                                             AttributeCopy::range(dst_offset, 0, count));
+                    R.tetrahedra().copy_from(
+                        complex->tetrahedra(),
+                        AttributeCopy::range(tetrahedron_offsets[I], 0, count));
                 }
-                dst_offset += count;
             }
         }
 
