a46a0534eaf388d50faa5881dc76c1a7c600a3f5
MuGdxy <lxy819469559@gmail.com>
2024-09-17 15:29:01 +0800

    SoftTransformConstraint pybind

---
+++ src/pyuipc/constitution/soft_transform_constraint.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <pyuipc/pyuipc.h>
+
+namespace pyuipc::constitution
+{
+class PySoftTransformConstraint
+{
+  public:
+    PySoftTransformConstraint(py::module& m);
+};
+}  // namespace pyuipc
