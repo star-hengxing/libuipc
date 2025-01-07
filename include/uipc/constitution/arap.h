47df467fd7e96dbe54fc89566822e55d3257af8f
luxinyu@transgpsim <lxy819469559@gmail.com>
2025-01-07 14:22:40 +0800

    export FEM ARAP pybind

--- include/uipc/constitution/arap.h
+++ include/uipc/constitution/arap.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <uipc/constitution/finite_element_constitution.h>
+#include <uipc/common/unit.h>
 
 namespace uipc::constitution
 {
@@ -10,7 +11,7 @@ class UIPC_CONSTITUTION_API ARAP : public FiniteElementConstitution
   public:
     ARAP(const Json& config = default_config()) noexcept;
 
-    void apply_to(geometry::SimplicialComplex& sc, Float kappa, Float mass_density = 1e3) const;
+    void apply_to(geometry::SimplicialComplex& sc, Float kappa = 1.0_MPa, Float mass_density = 1e3) const;
 
     static Json default_config() noexcept;
 
