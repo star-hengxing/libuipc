f906b7f487e1814d33f7b190c8d1c70264075735
MuGdxy <lxy819469559@gmail.com>
2025-01-08 21:25:34 +0800

    impl pybind for new contact tabular

--- include/uipc/core/contact_model_collection.h
+++ include/uipc/core/contact_model_collection.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <uipc/geometry/attribute_collection.h>
 
+namespace uipc::geometry
+{
+template <typename T>
+class AttributeFriend;
+}
+
 namespace uipc::core
 {
 class ContactTabular;
@@ -8,6 +14,9 @@ class ContactTabular;
 template <bool IsConst>
 class ContactModelCollectionT
 {
+    template <typename T>
+    friend class geometry::AttributeFriend;
+
     friend class ContactTabular;
     using AutoAttributeCollection =
         std::conditional_t<IsConst, const geometry::AttributeCollection, geometry::AttributeCollection>;
@@ -31,6 +40,8 @@ class ContactModelCollectionT
     {
         return m_attributes.template find<T>(name);
     }
+
+    auto find(std::string_view name) { return m_attributes.find(name); }
 
     auto to_json() const { return m_attributes.to_json(); }
 
