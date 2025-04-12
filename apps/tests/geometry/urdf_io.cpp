7dd54d2f4f8061c97f4b89a5d110aecb597718fa
MuGdxy <lxy819469559@gmail.com>
2025-04-12 16:35:05 +0800

    add urdf deps

---
+++ apps/tests/geometry/urdf_io.cpp
@@ -0,0 +1,9 @@
+#include <catch.hpp>
+#include <app/asset_dir.h>
+#include <uipc/uipc.h>
+#include <uipc/io/urdf_io.h>
+
+using namespace uipc;
+using namespace uipc::geometry;
+
+TEST_CASE("read_urdf", "[io]") {}
