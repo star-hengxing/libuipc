cf60f8f7eb80f726449b214c6c6c82cac0a2945b
MuGdxy <lxy819469559@gmail.com>
2025-06-01 14:44:35 +0800

    remove boost core deps

--- src/core/common/abort.cpp
+++ src/core/common/abort.cpp
@@ -5,8 +5,8 @@ namespace uipc
 {
 void abort() noexcept
 {
-    auto raw_trace = cpptrace::generate_raw_trace(1);
-    raw_trace.resolve().print_with_snippets();
-    std::abort();
+    auto raw_trace = cpptrace::generate_raw_trace(1 /*skip this `abort` function*/);
+    raw_trace.resolve().print_with_snippets();  // better error messages
+    std::abort();  // call std::abort to terminate the program
 }
 }  // namespace uipc
